a79d00cdf688b674377c02e6fde0e608eaa8e981
Santo Rasu <santorasu09.com>
2025-01-17 22:12:53 +0600

    This is initial files of flutter

---
+++ android/app/src/main/kotlin/com/santo/cgpa_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.santo.cgpa_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(myApp());
+}
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
